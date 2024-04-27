395b39339271e4cce7a8351fd24d6f84a1eb525c
KONOHA-GANGSTA <piligrim507@gmail.com>
2024-04-27 17:55:17 +0300

    first commit

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,19 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
+console.log(encoded, translations);
 
+const decoded = encoded.map((element) =>
+  Object.entries(element).reduce((acc, cur) => {
+    const [key, val] = cur;
+    if (
+      key.slice(key.length - 2) === "Id" &&
+      Object.keys(translations).includes(val)
+    )
+      acc[key] = translations[val];
+    else acc[key] = val;
+    return acc;
+  }, {})
+);
 
-
-// console.log(decoded)
+console.log(decoded);
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,80 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import CurrencyInput from "./components/CurrencyInput/CurrencyInput.svelte";
+import { getConvRates } from "./utils/getConvRates";
+  let rates = getConvRates("USD")
+  let currency1 = "USD"
+  let currency2 = "USD"
+  let input1 = 1
+  let input2 = 1
+
+  interface CurrencyDetail {
+    currency: string;
+    value: number;
+  }
+
+  interface Rates {
+    [currency: string]: number;
+  }
+
+
+  const upperUpdateValue = (event: CustomEvent<CurrencyDetail>,dict:Rates)=>{
+    input2= event.detail.value * dict[currency2];
+  }
+
+  const lowerUpdateValue = (event: CustomEvent<CurrencyDetail>,dict:Rates)=>{
+    input1= event.detail.value / dict[currency2];
+  }
+
+  const upperUpdateCurency = (event: CustomEvent<CurrencyDetail>,dict:Rates)=>{
+    currency1 = event.detail.currency
+    rates = getConvRates(event.detail.currency)
+    input1 = input2/dict[event.detail.currency]
+  }
+
+  const lowerUpdateCurrency = (event: CustomEvent<CurrencyDetail>,dict:Rates)=>{
+    currency2 = event.detail.currency
+    input2 = input1*dict[event.detail.currency]
+  }
+
 </script>
 
+<div class="header">
+  КОНВЕРТЕР ВАЛЮТ
+</div>
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="currencyConverter">
+    {#await rates}
+      <div>LOADING...</div>
+    {:then rates}
+      <CurrencyInput
+       input_value={input1} 
+       selected={currency1} 
+       rates={rates}
+       on:updateValue={(ev)=>upperUpdateValue(ev,rates)}
+       on:updateCurrency={(ev)=>upperUpdateCurency(ev,rates)}
+       />
+      <CurrencyInput 
+      input_value={input2} 
+      selected={currency2} 
+      rates={rates} 
+      on:updateValue={(ev)=>lowerUpdateValue(ev,rates)}
+      on:updateCurrency={(ev)=>lowerUpdateCurrency(ev,rates)}
+      />
+    {/await}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .currencyConverter{
+    display: flex;
+    flex-direction: column;
+    gap:20px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  
+  .header{
+    font-size: 64px;
+    text-align: center;
+    margin-bottom: 50px;
   }
 </style>
